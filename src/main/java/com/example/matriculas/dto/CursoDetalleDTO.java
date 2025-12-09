fbc635f744febbbdab1acc834b6f17db33614fb0
AlejandroRuidias <alejandro.ruidias.14@gmail.com>
2025-12-09 03:01:32 -0500

    fix: eliminacion y agregacion de cursos que puede dictar un docente

--- src/main/java/com/example/matriculas/dto/CursoDetalleDTO.java
+++ src/main/java/com/example/matriculas/dto/CursoDetalleDTO.java
@@ -1,12 +1,16 @@
 package com.example.matriculas.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CursoDetalleDTO {
     private Long id;
     private String codigo;
